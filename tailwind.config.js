016bbe93480d91b9ef0fee3bda0968ae7ae4df10
unknown <aptiarahgozar@hotmail.com>
2023-11-10 22:18:48 +0330

    Complete Files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      screens: {
+        lgscreen: "1152px",
+      },
+    },
+  },
+  plugins: [],
+};
